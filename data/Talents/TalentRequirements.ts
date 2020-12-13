0ec4a30efcb7bb6818d838f7c5bb70f85dd00611
tswow-admin <tswow@protonmail.com>
2020-12-13 11:27:33 +0100

    Fix talent requirements

--- data/Talents/TalentRequirements.ts
+++ data/Talents/TalentRequirements.ts
@@ -1,5 +1,24 @@
+import { Cell } from "wotlkdata/cell/Cell";
 import { ArrayEntry, SystemArray } from "wotlkdata/cell/Systems/SystemArray";
 import { Talent } from "./Talent";
+
+export class TalentRankWrap extends Cell<number, Talent> {
+    protected index: number;
+
+    constructor(owner: Talent, index: number) {
+        super(owner);
+        this.index = index;
+    }
+
+    get(): number {
+        return this.owner.row.PrereqRank.getIndex(this.index);
+    }
+
+    set(value: number): Talent {
+        this.owner.row.PrereqRank.setIndex(this.index,value>0?value-1:0);
+        return this.owner;
+    }
+}
 
 export class TalentRequirement extends ArrayEntry<Talent> {
 
@@ -10,11 +29,11 @@ export class TalentRequirement extends ArrayEntry<Talent> {
     }
 
     isClear(): boolean {
-        return this.owner.row.PrereqTalent.getIndex(this.index) !== 0;
+        return this.owner.row.PrereqTalent.getIndex(this.index) === 0;
     }
 
     get Talent() { return this.ownerWrapIndex(this.owner.row.PrereqTalent, this.index); }
-    get Rank() { return this.ownerWrapIndex(this.owner.row.PrereqRank, this.index); }
+    get Rank() { return new TalentRankWrap(this.owner, this.index); }
 
     set(talent: number, rank: number) {
         this.Talent.set(talent);
