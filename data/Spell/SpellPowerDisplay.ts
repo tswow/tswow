2fc7bcc2a5e0c59a02d1528b1eb28323181eda19
tswow-admin <tswow@protonmail.com>
2020-12-13 14:20:43 +0100

    Add EnumFields to SpellPowerDisplay

--- data/Spell/SpellPowerDisplay.ts
+++ data/Spell/SpellPowerDisplay.ts
@@ -14,16 +14,31 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-import { Enum } from "wotlkdata/cell/Systems/Enum";
+import { Enum, EnumField } from "wotlkdata/cell/Systems/Enum";
 import { Spell } from "./Spell";
 
 export class SpellPowerDisplay extends Enum<Spell> {
+    @EnumField(0)
     setNone() { return this.set(0); }
+
+    @EnumField(1)
     setAmmoSlot() { return this.set(1); }
+
+    @EnumField(41)
     setPyrite() { return this.set(41); }
+
+    @EnumField(61)
     setSteam() { return this.set(61); }
+
+    @EnumField(101)
     setHeat() { return this.set(101); }
+
+    @EnumField(121)
     setOoze() { return this.set(121); }
+
+    @EnumField(141)
     setBloodPower() { return this.set(141); }
+
+    @EnumField(142)
     setWrath() { return this.set(142); }
 }
