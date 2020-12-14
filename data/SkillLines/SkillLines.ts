6844dae2102c43ab53657c3b35c2ab16f6c02e23
tswow-admin <tswow@protonmail.com>
2020-12-14 17:46:44 +0100

    Change class SkillLines to automatically be taught to new characters

--- data/SkillLines/SkillLines.ts
+++ data/SkillLines/SkillLines.ts
@@ -1,4 +1,5 @@
 import { DBC } from "wotlkdata"
+import { SQL } from "wotlkdata/sql/SQLFiles";
 import { Ids } from "../Base/Ids";
 import { SkillLine } from "./SkillLine"
 
@@ -12,7 +13,7 @@ export const SkillLines = {
     },
 
     createClass(mod: string, id: string, cls: number) {
-        return SkillLines.create(mod, id)
+        const sl = SkillLines.create(mod, id)
             .Category.set(7)
             .SkillCosts.set(0)
             .CanLink.set(0)
@@ -21,7 +22,7 @@ export const SkillLines = {
                 .ClassMask.mark(cls-1)
                 .Flags.set(1040)
                 .up()
+        SQL.playercreateinfo_skills.add(0, 1<<(cls-1),sl.ID).comment.set(`${cls} - ${id}`);
+        return sl;
     }
-
-
 }
