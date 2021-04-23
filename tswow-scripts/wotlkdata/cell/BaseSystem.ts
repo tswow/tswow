20829d164275aa9a62a07923661a70c62ea21d39
ihm-tswow <ihm-ts@protonmail.com>
2021-04-23 12:18:42 +0200

    datascripts: stop requiring basesystem type for SharedRef holders

--- tswow-scripts/wotlkdata/cell/BaseSystem.ts
+++ tswow-scripts/wotlkdata/cell/BaseSystem.ts
@@ -1,11 +1,16 @@
 export class BaseSystem {
     private uniqueRefs: boolean = true;
 
-    static getUniqueRefs(system: BaseSystem) {
+    static getUniqueRefs(system: any) {
+        if(!system) return true;
+        if(typeof(system)!='object') throw new Error(`Tried using non-object as owner`);
+        if(system.uniqueRefs === undefined) return true;
         return system.uniqueRefs;
     }
 
     static setUniqueRefs(system: BaseSystem, uniqueRefs: boolean) {
+        if(!system) throw new Error(`Tried setting unique refs on null object`);
+        if(typeof(system)!=='object') throw new Error(`Tried setting unique refs on non-object`);
         system.uniqueRefs = uniqueRefs;
     }
 }
