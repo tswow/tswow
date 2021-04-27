0eb6c06a38753264abb25b7bd2521229f72f26c1
ihm-tswow <ihm-ts@protonmail.com>
2021-04-27 10:27:51 +0200

    tswow: fix: make build types case insensitive

--- tswow-scripts/util/BuildType.ts
+++ tswow-scripts/util/BuildType.ts
@@ -3,10 +3,11 @@ export type BuildType = 'Release'|'Debug'|'RelWithDebInfo'
 export const DEFAULT_BUILD_TYPE: BuildType = 'RelWithDebInfo';
 
 export function findBuildType(args: string[], def: BuildType = DEFAULT_BUILD_TYPE) {
+    args = args.map(x=>x.toLowerCase());
     if(args.includes(def)) return def;
-    if(args.includes('Debug')) return 'Debug';
-    if(args.includes('Release')) return 'Release';
-    if(args.includes('RelWithDebInfo')) return 'RelWithDebInfo';
+    if(args.includes('debug')) return 'Debug';
+    if(args.includes('release')) return 'Release';
+    if(args.includes('relwithdebinfo')) return 'RelWithDebInfo';
     return def;
 }
 
