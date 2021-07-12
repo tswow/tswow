7d462fb53d9df95b834256af49414686145fe15b
tswow-admin <tswow@protonmail.com>
2021-07-12 09:38:36 +0200

    livescripts: add test system
    - add support for ad-hoc mysql prepared statements
    
    - move cs_tswow to tswow-core-module

--- tswow-scripts/sql/auth/tswow_rbac.sql
+++ tswow-scripts/sql/auth/tswow_rbac.sql
@@ -2,8 +2,10 @@
 INSERT IGNORE INTO `rbac_permissions` VALUES(17688,"Command: at");
 INSERT IGNORE INTO `rbac_permissions` VALUES(17689,"Command: clearat");
 INSERT IGNORE INTO `rbac_permissions` VALUES(17690,"Command: id");
+INSERT IGNORE INTO `rbac_permissions` VALUES(17691,"Command: test");
 
 -- Makes these commands available to administrators
 INSERT IGNORE INTO `rbac_linked_permissions` VALUES(192,17688);
 INSERT IGNORE INTO `rbac_linked_permissions` VALUES(192,17689);
 INSERT IGNORE INTO `rbac_linked_permissions` VALUES(192,17690);
+INSERT IGNORE INTO `rbac_linked_permissions` VALUES(192,17691);
