eff905c5f49f460d871153055644a557a122bcf8
ihm-tswow <ihm-ts@protonmail.com>
2021-09-15 17:38:25 +0200

    datascripts: improve EnumCell code duplication

--- tswow-scripts/wotlkdata/cell/cells/EnumCell.ts
+++ tswow-scripts/wotlkdata/cell/cells/EnumCell.ts
@@ -1,10 +1,11 @@
 import { Objectified, Objects } from "../serialization/ObjectIteration";
 import { CellSystemTop } from "../systems/CellSystem";
 import { Cell, CellWrapper } from "./Cell";
+import { CellReadOnly, CellWrapperReadOnly } from "./CellReadOnly";
 
-export class EnumCell<T> extends CellWrapper<number,T> {
-    value(index: number, setCallback?: ()=>void): EnumValue<T> {
-        return new EnumValue(this.owner, this, index, setCallback);
+export class EnumCellReadOnly<T> extends CellWrapperReadOnly<number,T> {
+    value(index: number): EnumValueReadOnly<T> {
+        return new EnumValueReadOnly(this.owner, this, index);
     }
 
     objectify() {
@@ -16,31 +17,70 @@ export class EnumCell<T> extends CellWrapper<number,T> {
     }
 }
 
-export class EnumValue<T> {
-    private owner: T;
-    private cell: Cell<number,any>
-    private index: number;
+export class EnumValueReadOnly<T> {
+    protected owner: T;
+    protected cell: CellReadOnly<number,any>
+    protected index: number;
     protected get isEnum() { return true; }
-    private setCallback?: ()=>void
 
-    constructor(owner: T, cell: EnumCell<T>, index: number, setCallback?: ()=>void) {
+    constructor(owner: T, cell: EnumCellReadOnly<T>, index: number) {
         this.owner = owner;
         this.cell = cell;
         this.index = index;
-        this.setCallback = setCallback;
     }
 
     isSelected() {
         return this.cell.get() === this.index;
     }
 
+    protected set() {
+        CellReadOnly.set(this.cell,this.index);
+        return this.owner;
+    }
+}
+
+export class EnumCell<T> extends EnumCellReadOnly<T> {
+    value(index: number, setCallback?: ()=>void): EnumValue<T> {
+        return new EnumValue(this.owner, this, index, setCallback);
+    }
+
+    set(value: number) { return super.set(value); }
+}
+
+export class EnumValue<T> extends EnumValueReadOnly<T>{
+    protected get isEnum() { return true; }
+    private setCallback?: ()=>void
+
+    constructor(owner: T, cell: EnumCell<T>, index: number, setCallback?: ()=>void) {
+        super(owner,cell,index);
+        this.setCallback = setCallback;
+    }
+
     set() {
-        this.cell.set(this.index);
+        super.set();
         if(this.setCallback) this.setCallback();
         return this.owner;
     }
 }
 
+function enumCellTransformGetSelection(transform: any) {
+    return Object
+        .entries(
+            Objects.mapObject(transform, ['object'], (_,v)=>v.isEnum)
+        )
+        .map(([name,cell])=>({name,cell}))
+        .find(({name,cell})=>cell.isSelected())
+        || {name:undefined,cell:undefined}
+}
+
+function verifyAs(value1: number, value2: number) {
+    if(value1 !== value2) {
+        throw new Error(
+            `Tried accessing enum as ${value1},`
+          + ` but value is ${value2}`
+      )
+    }
+}
 
 export class EnumCellTransform<T extends Objectified> extends CellWrapper<number,T> {
     value<V extends Objectified>(index: number, transformer: (t: T)=>V): EnumValueTransform<T,V> {
@@ -56,11 +96,7 @@ export class EnumCellTransform<T extends Objectified> extends CellWrapper<number
             , cell: EnumValueTransform<T,any>|undefined
         }
     {
-        let enums = Objects.mapObject(this,['object'],(k,v)=>v.isEnum);
-        for(const [name,cell] of Object.entries(enums)) {
-            if(cell.isSelected()) return {name,cell};
-        }
-        return {name:undefined,cell:undefined};
+        return enumCellTransformGetSelection(this);
     }
 
     objectify() {
@@ -97,12 +133,7 @@ export class EnumValueTransform<T extends Objectified,V extends Objectified> {
     }
 
     as() {
-        if(this.cell.get() !== this.index) {
-            throw new Error(
-                  `Tried accessing enum as ${this.index},`
-                + ` but value is ${this.cell.get()}`
-            )
-        }
+        verifyAs(this.index, this.cell.get())
         return this.transformer(this.owner);
     }
 }
@@ -117,6 +148,74 @@ export abstract class TransformedClass<T> extends CellSystemTop {
 
     objectify() {
         let {cell} = EnumCellTransform.getSelection(this.transformer());
+        if(cell === undefined) {
+            return this.objectifyParent();
+        }
+        return cell.as().objectifyParent();
+    }
+}
+
+export class EnumCellTransformReadOnly<T extends Objectified> extends CellWrapperReadOnly<number,T> {
+    value<V extends Objectified>(index: number, transformer: (t: T)=>V): EnumValueTransformReadOnly<T,V> {
+        return new EnumValueTransformReadOnly(this.owner, this, index, transformer);
+    }
+
+    plain_value(index: number) {
+        return new EnumValueTransformReadOnly(this.owner, this, index, (t)=>t);
+    }
+
+    private getSelection():
+        {     name: string|undefined
+            , cell: EnumValueTransformReadOnly<T,any>|undefined
+        }
+    {
+        return enumCellTransformGetSelection(this)
+    }
+
+    objectify() {
+        const {name} = this.getSelection();
+        return name === undefined ? this.cell.get() : name;
+    }
+
+    static getSelection(transformEnum: EnumCellTransformReadOnly<any>) {
+        return transformEnum.getSelection();
+    }
+}
+
+export class EnumValueTransformReadOnly<T extends Objectified,V extends Objectified> {
+    private owner: T;
+    private cell: CellReadOnly<number,any>
+    private index: number;
+    protected get isEnum() { return true; }
+    protected transformer: (t: T)=>V;
+
+    constructor(owner: T, cell: EnumCellTransformReadOnly<T>, index: number, transformer: (t: T)=>V) {
+        this.owner = owner;
+        this.cell = cell;
+        this.index = index;
+        this.transformer = transformer;
+    }
+
+    isSelected() {
+        return this.cell.get() === this.index;
+    }
+
+    as() {
+        verifyAs(this.index, this.cell.get())
+        return this.transformer(this.owner);
+    }
+}
+
+export abstract class TransformedClassReadOnly<T> extends CellSystemTop {
+    protected abstract transformer(): EnumCellTransformReadOnly<any>
+    protected abstract default(): T;
+
+    protected objectifyParent() {
+        return Objects.objectifyObj(this);
+    }
+
+    objectify() {
+        let {cell} = EnumCellTransformReadOnly.getSelection(this.transformer());
         if(cell === undefined) {
             return this.objectifyParent();
         }
