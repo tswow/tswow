094617760e20a6b35c1cd276d54b83cff5e937bd
tswow-admin <tswow@protonmail.com>
2021-09-30 11:13:23 +0200

    datascripts: add Team enum literals

--- datascripts/Misc/TeamEnum.ts
+++ datascripts/Misc/TeamEnum.ts
@@ -9,3 +9,24 @@ export class TeamEnumReadOnly<T> extends EnumCellReadOnly<T> {
     get Horde() { return this.value(67); }
     get Alliance() { return this.value(469); }
 }
+
+export const TEAMS = [
+    'HORDE'
+  , 'ALLIANCE'
+  , 'ANY'
+] as const
+
+export type Team = typeof TEAMS[number] | number
+
+export function resolveTeam(team: Team) {
+    if(typeof(team) === 'number') {
+        return team;
+    }
+
+    switch(team) {
+        case 'ALLIANCE': return 469;
+        case 'HORDE': return 67;
+        case 'ANY': return 0;
+        default: throw new Error(`Invalid team: ${team}`)
+    }
+}
