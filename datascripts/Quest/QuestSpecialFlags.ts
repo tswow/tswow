155d9f7a75a19ff4a5d92b380c5191b9afd3a618
ihm-tswow <ihm-ts@protonmail.com>
2021-10-03 21:41:15 +0200

    datascripts: fix quest special flags

--- datascripts/Quest/QuestSpecialFlags.ts
+++ datascripts/Quest/QuestSpecialFlags.ts
@@ -2,7 +2,7 @@ import { MaskCell32 } from "wotlkdata/cell/cells/MaskCell";
 import { Quest } from "./Quest";
 
 export class QuestSpecialFlags extends MaskCell32<Quest> {
-    get Repeatable() { return this.bit(0); }
+    get Repeatable()      { return this.bit(0); }
 
     /**
      * Makes quest only completable via some external event,
@@ -11,13 +11,13 @@ export class QuestSpecialFlags extends MaskCell32<Quest> {
      * - entry in areatrigger_involvedrelation
      * - entry in spell_script with command 7
      */
-    get CustomComplete() { return this.bit(0); }
-    get AutoAccept() { return this.bit(0); }
-    get DungeonFinder() { return this.bit(0); }
-    get Monthly() { return this.bit(0); }
+    get CustomComplete()  { return this.bit(1); }
+    get AutoAccept()      { return this.bit(2); }
+    get DungeonFinder()   { return this.bit(3); }
+    get Monthly()         { return this.bit(4); }
 
     /**
      * usually involves killing invisible "bunny" npc with a spell
      */
-    get DummyKillCredit() { return this.bit(0); }
+    get DummyKillCredit() { return this.bit(5); }
 }
