70ca42f3f8931e6d961b398aa064df98d2c95a11
tswow-admin <tswow@protonmail.com>
2021-10-06 13:08:07 +0200

    datascripts: add AreaTriggerTeleport

--- datascripts/AreaTrigger/AreaTrigger.ts
+++ datascripts/AreaTrigger/AreaTrigger.ts
@@ -7,10 +7,12 @@ import { Ids } from "../Misc/Ids";
 import { distance3d, Position } from "../Misc/Position";
 import { PositionMapXYZCell } from "../Misc/PositionCell";
 import { AreaTriggerTavern } from "./AreaTriggerTavern";
+import { AreaTriggerTeleport } from "./AreaTriggerTeleport";
 
 export class AreaTriggerBase extends MainEntity<AreaTriggerRow> {
     get ID() { return this.row.ID.get(); }
     get Tavern() { return new AreaTriggerTavern(this); }
+    get Teleport() { return new AreaTriggerTeleport(this); }
     get Position() {
         return new PositionMapXYZCell(
               this
