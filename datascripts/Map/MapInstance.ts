57d15e2edb0560b68c076dca74d3c58455f428c8
ihm-tswow <ihm-ts@protonmail.com>
2021-10-08 08:14:22 +0200

    datascripts: addMapInstance#Script property

--- datascripts/Map/MapInstance.ts
+++ datascripts/Map/MapInstance.ts
@@ -1,8 +1,29 @@
+import { CellWrapper } from "wotlkdata/cell/cells/Cell";
 import { SQL } from "wotlkdata/sql/SQLFiles";
 import { instance_templateRow } from "wotlkdata/sql/types/instance_template";
 import { BoolCell } from "../Misc/BoolCell";
 import { MaybeSQLEntity } from "../Misc/SQLDBCEntity";
 import { Map } from "./Map";
+
+// also defined in TrinityCore/src/server/game/Scripting/ScriptMgr.h
+export const CUSTOM_SCRIPT_NAME = 'custom_script'
+
+export class MapInstanceScriptCell extends CellWrapper<string,Map> {
+    get(): string {
+        return this.cell.get();
+    }
+    set(value: string): Map {
+        this.cell.set(value);
+        return this.owner;
+    }
+
+    /**
+     * Enables the default 'custom instance' for this dungeon
+     */
+    setCustom() {
+        return this.set(CUSTOM_SCRIPT_NAME)
+    }
+}
 
 export class MapInstance extends MaybeSQLEntity<Map,instance_templateRow> {
     protected createSQL(): instance_templateRow {
@@ -19,5 +40,12 @@ export class MapInstance extends MaybeSQLEntity<Map,instance_templateRow> {
 
     get AllowMount() {
         return new BoolCell(this.owner, this.wrapSQL(0, sql=>sql.allowMount))
+    }
+
+    get Script() {
+        return new MapInstanceScriptCell(
+              this.owner
+            , this.wrapSQL('', sql=>sql.script)
+        );
     }
 }
