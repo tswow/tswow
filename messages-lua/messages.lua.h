9b252009b1a74fd222fb3a377bfa57d54a1d59d9
ihm-tswow <ihm-ts@protonmail.com>
2021-10-17 09:41:05 +0200

    update messages.lua.h
    - just commit it, makes it so users don't get errors before they compile

--- messages-lua/messages.lua.h
+++ messages-lua/messages.lua.h
@@ -1,3 +1,8 @@
 // auto-generated file, do not edit
 #pragma once
-#define MESSAGES_LUA ""
+#define MESSAGES_LUA \
+"-- This api assumes 'registerMessages' has been called" \
+"" \
+"function WriteMessage(size)" \
+"	-- TODO: implement" \
+"end" \
