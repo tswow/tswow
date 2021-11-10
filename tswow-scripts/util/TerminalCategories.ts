cb2920a038c5aeb7bc64baefd481d55718aa1982
ihm-tswow <ihm-ts@protonmail.com>
2021-11-10 11:27:57 +0100

    shell: allow custom terminal categories without parent

--- tswow-scripts/util/TerminalCategories.ts
+++ tswow-scripts/util/TerminalCategories.ts
@@ -2,9 +2,15 @@ export class CustomCategory {
     orig: TerminalCategory
     custom: string;
 
-    constructor(orig: TerminalCategory, custom: string) {
+    constructor(orig: TerminalCategory|undefined, custom: string) {
         this.orig = orig;
         this.custom = custom;
+    }
+
+    toString() {
+        return this.orig !== undefined
+            ? `${getTerminalCategory(this.orig)}/${this.custom}`
+            : this.custom
     }
 }
 
@@ -24,10 +30,16 @@ export type TerminalCategory =
     | 'client'
     | CustomCategory
 
-export function termCustom(cat: TerminalCategory, custom: string) {
-    return new CustomCategory(cat,custom);
+export function termCustom(custom: string);
+export function termCustom(cat: TerminalCategory, custom: string);
+export function termCustom(catOrCustom: TerminalCategory|string, custom?: string) {
+    if(custom === undefined) {
+        return new CustomCategory(undefined,catOrCustom as string);
+    } else {
+        return new CustomCategory(catOrCustom as TerminalCategory,custom)
+    }
 }
 
 export function getTerminalCategory(cat: TerminalCategory) {
-    return typeof(cat) === 'string' ? cat : `${cat.orig}/${cat.custom}`
+    return typeof(cat) === 'string' ? cat : cat.toString()
 }
