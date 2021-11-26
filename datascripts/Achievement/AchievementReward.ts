5828cce4900133b8151ad09cc37427c9087000d7
ihm-tswow <ihm-ts@protonmail.com>
2021-11-26 11:53:42 +0100

    datascripts: improve AchievementRewards
    - change parent to MaybSQLEntity
    
    - add AchievementReward setter

--- datascripts/Achievement/AchievementReward.ts
+++ datascripts/Achievement/AchievementReward.ts
@@ -1,6 +1,7 @@
 import { Cell, CPrim } from "wotlkdata/wotlkdata/cell/cells/Cell";
 import { CellSystem } from "wotlkdata/wotlkdata/cell/systems/CellSystem";
 import { Language } from "wotlkdata/wotlkdata/dbc/Localization";
+import { loc_constructor } from "wotlkdata/wotlkdata/primitives";
 import { SQL } from "wotlkdata/wotlkdata/sql/SQLFiles";
 import { achievement_rewardRow } from "wotlkdata/wotlkdata/sql/types/achievement_reward";
 import { MaybeSQLEntity } from "../Misc/SQLDBCEntity";
@@ -37,6 +38,28 @@ export class AchievementRewardMail extends CellSystem<Achievement> {
     };
     get MailTemplate() {
         return AchievementReward.wrap(this.owner,0,sql=>sql.MailTemplateID)
+    }
+
+    set(sender: number, mailTemplate: number): Achievement;
+    set(sender: number, subject: loc_constructor|string, body: loc_constructor|string): Achievement;
+    set(sender: number, subjectOrMail: loc_constructor|number|string, body?: loc_constructor|string) {
+        this.Sender.set(sender);
+        if(typeof(subjectOrMail) === 'number') {
+            this.MailTemplate.set(subjectOrMail)
+        } else {
+            if(typeof(subjectOrMail) === 'string') {
+                this.Subject.enGB.set(subjectOrMail)
+            } else {
+                this.Subject.set(subjectOrMail)
+            }
+
+            if(typeof(body) === 'string') {
+                this.Body.enGB.set(body)
+            } else if(body !== undefined) {
+                this.Body.set(body)
+            }
+        }
+        return this.owner;
     }
 }
 
@@ -89,4 +112,5 @@ export class AchievementReward extends MaybeSQLEntity<Achievement,achievement_re
     get Mail() { return new AchievementRewardMail(this.owner); }
     get Item() { return this.wrapSQL(0,sql=>sql.ItemID)}
     get Title() { return new AchievementRewardTitle(this.owner); }
+    get Description() { return this.ownerWrapLoc(this.owner.row.Reward); }
 }
