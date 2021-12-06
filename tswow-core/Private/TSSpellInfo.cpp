3006834a5ba3a7a7963de4a541103e7c9ff9088b
ihm-tswow <ihm-ts@protonmail.com>
2021-12-06 16:21:42 +0100

    livescripts: add Get prefix to TSSpellInfo methods

--- tswow-core/Private/TSSpellInfo.cpp
+++ tswow-core/Private/TSSpellInfo.cpp
@@ -35,316 +35,316 @@ uint32 TSSpellInfo::ID()  {
     return info->Id;
 }
 
-uint32 TSSpellInfo::School()
+uint32 TSSpellInfo::GetSchool()
 {
     return info->SchoolMask;
 }
-uint32 TSSpellInfo::BaseLevel()
+uint32 TSSpellInfo::GetBaseLevel()
 {
     return info->BaseLevel;
 }
 
-uint32 TSSpellInfo::DmgClass()
+uint32 TSSpellInfo::GetDmgClass()
 {
     return info->DmgClass;
 }
 
-uint32 TSSpellInfo::ActiveIconID()
+uint32 TSSpellInfo::GetActiveIconID()
 {
     return info->ActiveIconID;
 }
 
-uint32 TSSpellInfo::AreaGroupID()
+uint32 TSSpellInfo::GetAreaGroupID()
 {
     return info->AreaGroupId;
 }
 
-uint32 TSSpellInfo::Attributes()
+uint32 TSSpellInfo::GetAttributes()
 {
     return info->Attributes;
 }
 
-uint32 TSSpellInfo::AttributesCu()
+uint32 TSSpellInfo::GetAttributesCu()
 {
     return info->AttributesCu;
 }
 
-uint32 TSSpellInfo::AttributesEx()
+uint32 TSSpellInfo::GetAttributesEx()
 {
     return info->AttributesEx;
 }
 
-uint32 TSSpellInfo::AttributesEx2()
+uint32 TSSpellInfo::GetAttributesEx2()
 {
     return info->AttributesEx2;
 }
 
-uint32 TSSpellInfo::AttributesEx3()
+uint32 TSSpellInfo::GetAttributesEx3()
 {
     return info->AttributesEx3;
 }
 
-uint32 TSSpellInfo::AttributesEx4()
+uint32 TSSpellInfo::GetAttributesEx4()
 {
     return info->AttributesEx4;
 }
 
-uint32 TSSpellInfo::AttributesEx5()
+uint32 TSSpellInfo::GetAttributesEx5()
 {
     return info->AttributesEx5;
 }
 
-uint32 TSSpellInfo::AttributesEx6()
+uint32 TSSpellInfo::GetAttributesEx6()
 {
     return info->AttributesEx6;
 }
 
-uint32 TSSpellInfo::AttributesEx7()
+uint32 TSSpellInfo::GetAttributesEx7()
 {
     return info->AttributesEx7;
 }
 
-uint32 TSSpellInfo::AuraInterruptFlags()
+uint32 TSSpellInfo::GetAuraInterruptFlags()
 {
     return info->AuraInterruptFlags;
 }
 
-uint32 TSSpellInfo::CasterAuraSpell()
+uint32 TSSpellInfo::GetCasterAuraSpell()
 {
     return info->CasterAuraSpell;
 }
 
-uint32 TSSpellInfo::CasterAuraState()
+uint32 TSSpellInfo::GetCasterAuraState()
 {
     return info->CasterAuraState;
 }
 
-uint32 TSSpellInfo::CasterAuraStateNot()
+uint32 TSSpellInfo::GetCasterAuraStateNot()
 {
     return info->CasterAuraStateNot;
 }
 
-uint32 TSSpellInfo::CategoryRecoveryTime()
+uint32 TSSpellInfo::GetCategoryRecoveryTime()
 {
     return info->CategoryRecoveryTime;
 }
 
-uint32 TSSpellInfo::ChannelInterruptFlags()
+uint32 TSSpellInfo::GetChannelInterruptFlags()
 {
     return info->ChannelInterruptFlags;
 }
 
-uint32 TSSpellInfo::Dispel()
+uint32 TSSpellInfo::GetDispel()
 {
     return info->Dispel;
 }
 
-uint32 TSSpellInfo::EquippedItemClass()
+uint32 TSSpellInfo::GetEquippedItemClass()
 {
     return info->EquippedItemClass;
 }
 
-uint32 TSSpellInfo::EquippedItemInventoryTypeMask()
+uint32 TSSpellInfo::GetEquippedItemInventoryTypeMask()
 {
     return info->EquippedItemInventoryTypeMask;
 }
 
-uint32 TSSpellInfo::EquippedItemSubClassMask()
+uint32 TSSpellInfo::GetEquippedItemSubClassMask()
 {
     return info->EquippedItemSubClassMask;
 }
 
-uint32 TSSpellInfo::ExcludeCasterAuraSpell()
+uint32 TSSpellInfo::GetExcludeCasterAuraSpell()
 {
     return info->ExcludeCasterAuraSpell;
 }
 
-uint32 TSSpellInfo::ExcludeTargetAuraSpell()
+uint32 TSSpellInfo::GetExcludeTargetAuraSpell()
 {
     return info->ExcludeTargetAuraSpell;
 }
 
-uint32 TSSpellInfo::ExplicitTargetMask()
+uint32 TSSpellInfo::GetExplicitTargetMask()
 {
     return info->ExplicitTargetMask;
 }
 
-uint32 TSSpellInfo::FacingCasterFlags()
+uint32 TSSpellInfo::GetFacingCasterFlags()
 {
     return info->FacingCasterFlags;
 }
 
-uint32 TSSpellInfo::InterruptFlags()
+uint32 TSSpellInfo::GetInterruptFlags()
 {
     return info->InterruptFlags;
 }
 
-uint32 TSSpellInfo::ManaCost()
+uint32 TSSpellInfo::GetManaCost()
 {
     return info->ManaCost;
 }
 
-uint32 TSSpellInfo::ManaCostPercentage()
+uint32 TSSpellInfo::GetManaCostPercentage()
 {
     return info->ManaCostPercentage;
 }
 
-uint32 TSSpellInfo::ManaCostPerlevel()
+uint32 TSSpellInfo::GetManaCostPerlevel()
 {
     return info->ManaCostPerlevel;
 }
 
-uint32 TSSpellInfo::ManaPerSecond()
+uint32 TSSpellInfo::GetManaPerSecond()
 {
     return info->ManaPerSecond;
 }
 
-uint32 TSSpellInfo::ManaPerSecondPerLevel()
+uint32 TSSpellInfo::GetManaPerSecondPerLevel()
 {
     return info->ManaPerSecondPerLevel;
 }
 
-uint32 TSSpellInfo::MaxAffectedTargets()
+uint32 TSSpellInfo::GetMaxAffectedTargets()
 {
     return info->MaxAffectedTargets;
 }
 
-uint32 TSSpellInfo::MaxLevel()
+uint32 TSSpellInfo::GetMaxLevel()
 {
     return info->MaxLevel;
 }
 
-uint32 TSSpellInfo::MaxTargetLevel()
+uint32 TSSpellInfo::GetMaxTargetLevel()
 {
     return info->MaxTargetLevel;
 }
 
-uint32 TSSpellInfo::Mechanic()
+uint32 TSSpellInfo::GetMechanic()
 {
     return info->Mechanic;
 }
 
-uint32 TSSpellInfo::PowerType()
+uint32 TSSpellInfo::GetPowerType()
 {
     return info->PowerType;
 }
 
-uint32 TSSpellInfo::PreventionType()
+uint32 TSSpellInfo::GetPreventionType()
 {
     return info->PreventionType;
 }
 
-uint32 TSSpellInfo::Priority()
+uint32 TSSpellInfo::GetPriority()
 {
     return info->Priority;
 }
 
-uint32 TSSpellInfo::ProcChance()
+uint32 TSSpellInfo::GetProcChance()
 {
     return info->ProcChance;
 }
 
-uint32 TSSpellInfo::ProcCharges()
+uint32 TSSpellInfo::GetProcCharges()
 {
     return info->ProcCharges;
 }
 
-uint32 TSSpellInfo::ProcFlags()
+uint32 TSSpellInfo::GetProcFlags()
 {
     return info->ProcFlags;
 }
 
-uint32 TSSpellInfo::RecoveryTime()
+uint32 TSSpellInfo::GetRecoveryTime()
 {
     return info->RecoveryTime;
 }
 
-uint32 TSSpellInfo::RequiresSpellFocus()
+uint32 TSSpellInfo::GetRequiresSpellFocus()
 {
     return info->RequiresSpellFocus;
 }
 
-uint32 TSSpellInfo::RuneCostID()
+uint32 TSSpellInfo::GetRuneCostID()
 {
     return info->RuneCostID;
 }
 
-uint32 TSSpellInfo::SchoolMask()
+uint32 TSSpellInfo::GetSchoolMask()
 {
     return info->SchoolMask;
 }
 
-uint32 TSSpellInfo::Speed()
+uint32 TSSpellInfo::GetSpeed()
 {
     return info->Speed;
 }
 
-uint32 TSSpellInfo::SpellFamilyFlags()
+uint32 TSSpellInfo::GetSpellFamilyFlags()
 {
     return info->SpellFamilyFlags;
 }
 
-uint32 TSSpellInfo::SpellFamilyName()
+uint32 TSSpellInfo::GetSpellFamilyName()
 {
     return info->SpellFamilyName;
 }
 
-uint32 TSSpellInfo::SpellIconID()
+uint32 TSSpellInfo::GetSpellIconID()
 {
     return info->SpellIconID;
 }
 
-uint32 TSSpellInfo::SpellLevel()
+uint32 TSSpellInfo::GetSpellLevel()
 {
     return info->SpellLevel;
 }
 
-uint32 TSSpellInfo::StackAmount()
+uint32 TSSpellInfo::GetStackAmount()
 {
     return info->StackAmount;
 }
 
-uint32 TSSpellInfo::Stances()
+uint32 TSSpellInfo::GetStances()
 {
     return info->Stances;
 }
 
-uint32 TSSpellInfo::StancesNot()
+uint32 TSSpellInfo::GetStancesNot()
 {
     return info->StancesNot;
 }
 
-uint32 TSSpellInfo::StartRecoveryCategory()
+uint32 TSSpellInfo::GetStartRecoveryCategory()
 {
     return info->StartRecoveryCategory;
 }
 
-uint32 TSSpellInfo::StartRecoveryTime()
+uint32 TSSpellInfo::GetStartRecoveryTime()
 {
     return info->StartRecoveryTime;
 }
 
-uint32 TSSpellInfo::TargetAuraSpell()
+uint32 TSSpellInfo::GetTargetAuraSpell()
 {
     return info->TargetAuraSpell;
 }
 
-uint32 TSSpellInfo::TargetAuraState()
+uint32 TSSpellInfo::GetTargetAuraState()
 {
     return info->TargetAuraState;
 }
 
-uint32 TSSpellInfo::TargetAuraStateNot()
+uint32 TSSpellInfo::GetTargetAuraStateNot()
 {
     return info->TargetAuraStateNot;
 }
 
-uint32 TSSpellInfo::TargetCreatureType()
+uint32 TSSpellInfo::GetTargetCreatureType()
 {
     return info->TargetCreatureType;
 }
 
-uint32 TSSpellInfo::Targets()
+uint32 TSSpellInfo::GetTargets()
 {
     return info->Targets;
 }
