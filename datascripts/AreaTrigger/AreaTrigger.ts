959b7f51d10982b4920c1c77a127342b378ceb71
ihm-tswow <ihm-ts@protonmail.com>
2021-12-06 19:14:22 +0100

    datascripts: change AreaTriggerRegistry to a real registry class

--- datascripts/AreaTrigger/AreaTrigger.ts
+++ datascripts/AreaTrigger/AreaTrigger.ts
@@ -1,12 +1,14 @@
 import { sort } from "wotlkdata";
 import { CellSystem } from "wotlkdata/wotlkdata/cell/systems/CellSystem";
 import { DBC } from "wotlkdata/wotlkdata/dbc/DBCFiles";
-import { AreaTriggerRow } from "wotlkdata/wotlkdata/dbc/types/AreaTrigger";
+import { AreaTriggerQuery, AreaTriggerRow } from "wotlkdata/wotlkdata/dbc/types/AreaTrigger";
+import { Table } from "wotlkdata/wotlkdata/table/Table";
 import { getInlineID } from "../InlineScript/InlineScript";
 import { MainEntity } from "../Misc/Entity";
-import { Ids } from "../Misc/Ids";
+import { Ids, StaticIDGenerator } from "../Misc/Ids";
 import { distance3d, Position } from "../Misc/Position";
 import { PositionMapXYZCell } from "../Misc/PositionCell";
+import { RegistryStatic } from "../Refs/Registry";
 import { AreaTriggerTavern } from "./AreaTriggerTavern";
 import { AreaTriggerTeleport } from "./AreaTriggerTeleport";
 
@@ -56,33 +58,61 @@ export class AreaTriggerBox extends AreaTriggerBase {
     get Box() { return new AreaTriggerBoxBox(this); }
 }
 
-export const AreaTriggerRegistry = {
+export class AreaTriggerRegistryClass
+    extends RegistryStatic<
+          AreaTriggerBase
+        , AreaTriggerRow
+        , AreaTriggerQuery
+    >
+{
+    protected Table(): Table<any, any, AreaTriggerRow> & { add: (id: number) => AreaTriggerRow; } {
+        return DBC.AreaTrigger
+    }
+    protected IDs(): StaticIDGenerator {
+        return Ids.AreaTrigger
+    }
+    Clear(r: AreaTriggerBase, mod: string, id: string): void {
+        r.row
+            .MapID.set(0)
+            .Radius.set(0)
+            .X.set(0)
+            .Y.set(0)
+            .Z.set(0)
+            .Box_Height.set(0)
+            .Box_Length.set(0)
+            .Box_Width.set(0)
+            .Box_Yaw.set(0)
+    }
+    protected FindByID(id: number): AreaTriggerRow {
+        return DBC.AreaTrigger.query({ID:id})
+    }
+    ID(e: AreaTriggerBase): number {
+        return e.ID
+    }
+    protected EmptyQuery() {
+        return {}
+    }
+    protected Entity(r: AreaTriggerRow): AreaTriggerBase {
+        return new AreaTriggerBase(r);
+    }
+
     createRadius(mod: string, id: string, pos: Position, radius: number|Position) {
         if(typeof(radius)==='object') {
             radius = distance3d(pos,radius);
         }
-        return new AreaTriggerRadius(
-            DBC.AreaTrigger
-                .add(Ids.AreaTrigger.id(mod,id))
-                .Box_Height.set(0)
-                .Box_Length.set(0)
-                .Box_Width.set(0)
-                .Box_Yaw.set(0)
-            )
+        return new AreaTriggerRadius(this.create(mod,id).row)
             .Position.set(pos)
             .Radius.set(radius)
-    },
+    }
 
     createBox(mod: string, id: string, pos: Position, length: number, width: number, height: number) {
-        return new AreaTriggerBox(
-            DBC.AreaTrigger
-                .add(Ids.AreaTrigger.id(mod,id))
-                .Radius.set(0)
-            )
+        return new AreaTriggerBox(this.create(mod,id).row)
             .Position.set(pos)
             .Box.set(length,width,height,pos.o)
-    },
-}
+    }
+};
+
+export const AreaTriggerRegistry = new AreaTriggerRegistryClass();
 
 sort('AreaTrigger',()=>{
     DBC.AreaTrigger.binarySort(0,row=>row.MapID.get());
