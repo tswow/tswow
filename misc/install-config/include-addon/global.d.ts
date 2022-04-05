f78e06c38ddadd92ccbac0a1f07b686115489246
ihm-tswow <ihm-ts@protonmail.com>
2022-04-05 09:56:29 +0200

    addon: add GetNumSkillLines/GetSkillLineInfo declarations

--- misc/install-config/include-addon/global.d.ts
+++ misc/install-config/include-addon/global.d.ts
@@ -14009,6 +14009,33 @@ declare function UnitHealth(unitId: WoWAPI.UnitId): number;
 declare function UnitHealthMax(unitId: WoWAPI.UnitId): number;
 
 /**
+ * Returns the number of skill lines in the skill window, including headers.
+ * @see https://wowpedia.fandom.com/wiki/API_GetNumSkillLines
+ */
+declare function GetNumSkillLines(): number
+
+/**
+ * Returns information on a skill line/header.
+ * @argument index The index of a line in the skills window, can be a header or skill line. Indices can change depending on collapsed/expanded headers.
+ * @returns
+ * 1. skillName - string - Name of the skill line.
+ * 2. header - number - Returns 1 if the line is a header, nil otherwise.
+ * 3. isExpanded - number - Returns 1 if the line is a header and expanded, nil otherwise.
+ * 4. skillRank - number - The current rank for the skill, 0 if not applicable.
+ * 5. numTempPoints - number - Temporary points for the current skill.
+ * 6. skillModifier - number - Skill modifier value for the current skill.
+ * 7. skillMaxRank - number - The maximum rank for the current skill. If this is 1 the skill is a proficiency.
+ * 8. isAbandonable - number - Returns 1 if this skill can be unlearned, nil otherwise.
+ * 9. stepCost - number - Returns 1 if skill can be learned, nil otherwise.
+ * 10. rankCost - number - Returns 1 if skill can be trained, nil otherwise.
+ * 11. minLevel - number - Minimum level required to learn this skill.
+ * 12. skillCostType - number
+ * 13. skillDescription - string - Localized skill description text
+ * @tupleReturn
+ */
+declare function GetSkillLineInfo(index: number): [string,number,number,number,number,number,number,number,number,number,number,number,string]
+
+/**
  * Returns 1 if the unit is a player in your party, nil otherwise
  * @param unitId unitId who should be checked
  * @see https://wow.gamepedia.com/API_UnitInParty
