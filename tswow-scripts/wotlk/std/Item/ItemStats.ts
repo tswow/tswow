02c3b407211e8b9c14a18f5faf25da81c364dd57
ihm-tswow <ihm-ts@protonmail.com>
2022-05-03 14:45:30 +0200

    datascripts: fix ItemStat enum values
    - takes the enum directly from TrinityCore

--- tswow-scripts/wotlk/std/Item/ItemStats.ts
+++ tswow-scripts/wotlk/std/Item/ItemStats.ts
@@ -48,53 +48,54 @@ function b(owner: ItemTemplate) {
     ]
 }
 
-export enum Stat {
+export enum Stat
+{
     MANA                     = 0,
     HEALTH                   = 1,
-    AGILITY                  = 2,
-    STRENGTH                 = 3,
-    INTELLECT                = 4,
-    SPIRIT                   = 5,
-    STAMINA                  = 6,
-    DEFENSE_SKILL_RATING     = 7,
-    DODGE_RATING             = 8,
-    PARRY_RATING             = 9,
-    BLOCK_RATING             = 10,
-    HIT_MELEE_RATING         = 11,
-    HIT_RANGED_RATING        = 12,
-    HIT_SPELL_RATING         = 13,
-    CRIT_MELEE_RATING        = 14,
-    CRIT_RANGED_RATING       = 15,
-    CRIT_SPELL_RATING        = 16,
-    HIT_TAKEN_MELEE_RATING   = 17,
-    HIT_TAKEN_RANGED_RATING  = 18,
-    HIT_TAKEN_SPELL_RATING   = 19,
-    CRIT_TAKEN_MELEE_RATING  = 20,
-    CRIT_TAKEN_RANGED_RATING = 21,
-    CRIT_TAKEN_SPELL_RATING  = 22,
-    HASTE_MELEE_RATING       = 23,
-    HASTE_RANGED_RATING      = 24,
-    HASTE_SPELL_RATING       = 25,
-    HIT_RATING               = 26,
-    CRIT_RATING              = 27,
-    HIT_TAKEN_RATING         = 28,
-    CRIT_TAKEN_RATING        = 29,
-    RESILIENCE_RATING        = 30,
-    HASTE_RATING             = 31,
-    EXPERTISE_RATING         = 32,
-    ATTACK_POWER             = 33,
-    RANGED_ATTACK_POWER      = 34,
-    FERAL_ATTACK_POWER       = 35,
-    SPELL_HEALING_DONE       = 36,
-    SPELL_DAMAGE_DONE        = 37,
-    MANA_REGENERATION        = 38,
-    ARMOR_PENETRATION_RATING = 39,
-    SPELL_POWER              = 40,
-    HEALTH_REGEN             = 41,
-    SPELL_PENETRATION        = 42,
-    BLOCK_VALUE              = 43,
-
+    AGILITY                  = 3,
+    STRENGTH                 = 4,
+    INTELLECT                = 5,
+    SPIRIT                   = 6,
+    STAMINA                  = 7,
+    DEFENSE_SKILL_RATING     = 12,
+    DODGE_RATING             = 13,
+    PARRY_RATING             = 14,
+    BLOCK_RATING             = 15,
+    HIT_MELEE_RATING         = 16,
+    HIT_RANGED_RATING        = 17,
+    HIT_SPELL_RATING         = 18,
+    CRIT_MELEE_RATING        = 19,
+    CRIT_RANGED_RATING       = 20,
+    CRIT_SPELL_RATING        = 21,
+    HIT_TAKEN_MELEE_RATING   = 22,
+    HIT_TAKEN_RANGED_RATING  = 23,
+    HIT_TAKEN_SPELL_RATING   = 24,
+    CRIT_TAKEN_MELEE_RATING  = 25,
+    CRIT_TAKEN_RANGED_RATING = 26,
+    CRIT_TAKEN_SPELL_RATING  = 27,
+    HASTE_MELEE_RATING       = 28,
+    HASTE_RANGED_RATING      = 29,
+    HASTE_SPELL_RATING       = 30,
+    HIT_RATING               = 31,
+    CRIT_RATING              = 32,
+    HIT_TAKEN_RATING         = 33,
+    CRIT_TAKEN_RATING        = 34,
+    RESILIENCE_RATING        = 35,
+    HASTE_RATING             = 36,
+    EXPERTISE_RATING         = 37,
+    ATTACK_POWER             = 38,
+    RANGED_ATTACK_POWER      = 39,
+    //FERAL_ATTACK_POWER       = 40, not in 3.3
+    SPELL_HEALING_DONE       = 41,                 // deprecated
+    SPELL_DAMAGE_DONE        = 42,                 // deprecated
+    MANA_REGENERATION        = 43,
+    ARMOR_PENETRATION_RATING = 44,
+    SPELL_POWER              = 45,
+    HEALTH_REGEN             = 46,
+    SPELL_PENETRATION        = 47,
+    BLOCK_VALUE              = 48
 }
+
 
 export class ItemStat extends ArrayEntry<ItemTemplate> {
     get Type() {
@@ -138,91 +139,91 @@ export class ItemStats extends ArraySystem<ItemStat,ItemTemplate> {
         return this.owner;
     }
 
-    addMana(value: number) { return this.add(0, value)}
+    addMana(value: number) { return this.add(Stat.MANA, value)}
 
-    addHealth(value: number) { return this.add(1, value)}
+    addHealth(value: number) { return this.add(Stat.HEALTH, value)}
 
-    addAgility(value: number) { return this.add(3, value)}
+    addAgility(value: number) { return this.add(Stat.AGILITY, value)}
 
-    addStrength(value: number) { return this.add(4, value)}
+    addStrength(value: number) { return this.add(Stat.STRENGTH, value)}
 
-    addIntellect(value: number) { return this.add(5, value)}
+    addIntellect(value: number) { return this.add(Stat.INTELLECT, value)}
 
-    addSpirit(value: number) { return this.add(6, value)}
+    addSpirit(value: number) { return this.add(Stat.SPIRIT, value)}
 
-    addStamina(value: number) { return this.add(7, value)}
+    addStamina(value: number) { return this.add(Stat.STAMINA, value)}
 
-    addDefenseSkillRating(value: number) { return this.add(12, value)}
+    addDefenseSkillRating(value: number) { return this.add(Stat.DEFENSE_SKILL_RATING, value)}
 
-    addDodgeRating(value: number) { return this.add(13, value)}
+    addDodgeRating(value: number) { return this.add(Stat.DODGE_RATING, value)}
 
-    addParryRating(value: number) { return this.add(14, value)}
+    addParryRating(value: number) { return this.add(Stat.PARRY_RATING, value)}
 
-    addBlockRating(value: number) { return this.add(15, value)}
+    addBlockRating(value: number) { return this.add(Stat.BLOCK_RATING, value)}
 
-    addHitMeleeRating(value: number) { return this.add(16, value)}
+    addHitMeleeRating(value: number) { return this.add(Stat.HIT_MELEE_RATING, value)}
 
-    addHitRangedRating(value: number) { return this.add(17, value)}
+    addHitRangedRating(value: number) { return this.add(Stat.HIT_RANGED_RATING, value)}
 
-    addHitSpellRating(value: number) { return this.add(18, value)}
+    addHitSpellRating(value: number) { return this.add(Stat.HIT_SPELL_RATING, value)}
 
-    addCritMeleeRating(value: number) { return this.add(19, value)}
+    addCritMeleeRating(value: number) { return this.add(Stat.CRIT_MELEE_RATING, value)}
 
-    addCritRangedRating(value: number) { return this.add(20, value)}
+    addCritRangedRating(value: number) { return this.add(Stat.CRIT_RANGED_RATING, value)}
 
-    addCritSpellRating(value: number) { return this.add(21, value)}
+    addCritSpellRating(value: number) { return this.add(Stat.CRIT_SPELL_RATING, value)}
 
-    addHitTakenMeleeRating(value: number) { return this.add(22, value)}
+    addHitTakenMeleeRating(value: number) { return this.add(Stat.HIT_TAKEN_MELEE_RATING, value)}
 
-    addHitTakenRangedRating(value: number) { return this.add(23, value)}
+    addHitTakenRangedRating(value: number) { return this.add(Stat.HIT_TAKEN_RANGED_RATING, value)}
 
-    addHitTakenSpellRating(value: number) { return this.add(24, value)}
+    addHitTakenSpellRating(value: number) { return this.add(Stat.HIT_TAKEN_SPELL_RATING, value)}
 
-    addCritTakenMeleeRating(value: number) { return this.add(25, value)}
+    addCritTakenMeleeRating(value: number) { return this.add(Stat.CRIT_TAKEN_MELEE_RATING, value)}
 
-    addCritTakenRangedRating(value: number) { return this.add(26, value)}
+    addCritTakenRangedRating(value: number) { return this.add(Stat.CRIT_TAKEN_RANGED_RATING, value)}
 
-    addCritTakenSpellRating(value: number) { return this.add(27, value)}
+    addCritTakenSpellRating(value: number) { return this.add(Stat.CRIT_TAKEN_SPELL_RATING, value)}
 
-    addHasteMeleeRating(value: number) { return this.add(28, value)}
+    addHasteMeleeRating(value: number) { return this.add(Stat.HASTE_MELEE_RATING, value)}
 
-    addHasteRangedRating(value: number) { return this.add(29, value)}
+    addHasteRangedRating(value: number) { return this.add(Stat.HASTE_RANGED_RATING, value)}
 
-    addHasteSpellRating(value: number) { return this.add(30, value)}
+    addHasteSpellRating(value: number) { return this.add(Stat.HASTE_SPELL_RATING, value)}
 
-    addHitRating(value: number) { return this.add(31, value)}
+    addHitRating(value: number) { return this.add(Stat.HIT_RATING, value)}
 
-    addCritRating(value: number) { return this.add(32, value)}
+    addCritRating(value: number) { return this.add(Stat.CRIT_RATING, value)}
 
-    addHitTakenRating(value: number) { return this.add(33, value)}
+    addHitTakenRating(value: number) { return this.add(Stat.HIT_TAKEN_RATING, value)}
 
-    addCritTakenRating(value: number) { return this.add(34, value)}
+    addCritTakenRating(value: number) { return this.add(Stat.CRIT_TAKEN_RATING, value)}
 
-    addResilienceRating(value: number) { return this.add(35, value)}
+    addResilienceRating(value: number) { return this.add(Stat.RESILIENCE_RATING, value)}
 
-    addHasteRating(value: number) { return this.add(36, value)}
+    addHasteRating(value: number) { return this.add(Stat.HASTE_RATING, value)}
 
-    addExpertiseRating(value: number) { return this.add(37, value)}
+    addExpertiseRating(value: number) { return this.add(Stat.EXPERTISE_RATING, value)}
 
-    addAttackPower(value: number) { return this.add(38, value)}
+    addAttackPower(value: number) { return this.add(Stat.ATTACK_POWER, value)}
 
-    addRangedAttackPower(value: number) { return this.add(39, value)}
+    addRangedAttackPower(value: number) { return this.add(Stat.RANGED_ATTACK_POWER, value)}
 
-    addFeralAttackPower(value: number) { return this.add(40, value)}
+    //addFeralAttackPower(value: number) { return this.add(Stat.FERAL_ATTACK_POWER, value)}
 
-    addSpellHealingDone(value: number) { return this.add(41, value)}
+    addSpellHealingDone(value: number) { return this.add(Stat.SPELL_HEALING_DONE, value)}
 
-    addSpellDamageDone(value: number) { return this.add(42, value)}
+    addSpellDamageDone(value: number) { return this.add(Stat.SPELL_DAMAGE_DONE, value)}
 
-    addManaRegeneration(value: number) { return this.add(43, value)}
+    addManaRegeneration(value: number) { return this.add(Stat.MANA_REGENERATION, value)}
 
-    addArmorPenetrationRating(value: number) { return this.add(44, value)}
+    addArmorPenetrationRating(value: number) { return this.add(Stat.ARMOR_PENETRATION_RATING, value)}
 
-    addSpellPower(value: number) { return this.add(45, value)}
+    addSpellPower(value: number) { return this.add(Stat.SPELL_POWER, value)}
 
-    addHealthRegen(value: number) { return this.add(46, value)}
+    addHealthRegen(value: number) { return this.add(Stat.HEALTH_REGEN, value)}
 
-    addSpellPenetration(value: number) { return this.add(47, value)}
+    addSpellPenetration(value: number) { return this.add(Stat.SPELL_PENETRATION, value)}
 
-    addBlockValue(value: number) { return this.add(48, value)}
+    addBlockValue(value: number) { return this.add(Stat.BLOCK_VALUE, value)}
 }
