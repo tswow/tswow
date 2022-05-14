921ad3db5af5366ee86a433c83f5990b9269a77d
Kaytotes <me@kaytotes.online>
2022-05-14 12:34:53 +0100

    Enum

---
+++ tswow-scripts/wotlk/std/Quest/QuestInfoTypes.ts
@@ -0,0 +1,13 @@
+export enum QuestInfoTypes {
+    GROUP = 1,
+    LIFE = 21,
+    PVP = 41,
+    RAID = 62,
+    DUNGEON = 81,
+    WORLD_EVENT = 82,
+    LEGENDARY = 83,
+    ESCORT = 84,
+    HEROIC = 85,
+    RAID_10 = 88,
+    RAID_25 = 89,
+}
