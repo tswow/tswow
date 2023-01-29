824b0382b0b85b375a040428922a34a0dc6990d8
ihm-tswow <ihm-ts@protonmail.com>
2023-01-29 13:39:27 +0100

    livescripts: add missing TSItemEntry constructor

--- tswow-core/Private/TSItemEntry.cpp
+++ tswow-core/Private/TSItemEntry.cpp
@@ -1,5 +1,10 @@
 #include "TSItemEntry.h"
 
+TSItemEntry::TSItemEntry(uint32 entry, uint32 count)
+    : m_entry(entry)
+    , m_count(count)
+{}
+
 TSNumber<uint32> TSItemEntry::GetEntry()
 {
     return m_entry;
