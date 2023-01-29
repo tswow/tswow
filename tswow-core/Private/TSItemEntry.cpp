a9fafe46df8a262aa4fae183ea80ad07f05dfe16
ihm-tswow <ihm-ts@protonmail.com>
2023-01-29 11:47:02 +0100

    livescripts: fix TSItemEntry implementations

--- tswow-core/Private/TSItemEntry.cpp
+++ tswow-core/Private/TSItemEntry.cpp
@@ -1,11 +1,11 @@
 #include "TSItemEntry.h"
 
-uint32 TSItemEntry::GetEntry()
+TSNumber<uint32> TSItemEntry::GetEntry()
 {
     return m_entry;
 }
 
-uint32 TSItemEntry::GetCount()
+TSNumber<uint32> TSItemEntry::GetCount()
 {
     return m_count;
 }
