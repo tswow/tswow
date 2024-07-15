d4e7d6c482a7787b090737fa759e2b30c0f1c58f
tester <tester@tester.com>
2024-07-15 11:28:04 -0500

    new tdb path + submodule

--- tswow-scripts/util/Paths.ts
+++ tswow-scripts/util/Paths.ts
@@ -21,7 +21,7 @@ import { mpath, wfs } from './FileSystem';
 import { custom, dir, dirn, dynCustom, dyndir, dynfile, enumDir, file, FilePath, generateTree, WDirectory, WFile } from "./FileTree";
 import { isWindows } from './Platform';
 
-export const TDB_URL = "https://github.com/TrinityCore/TrinityCore/releases/download/TDB335.23061/TDB_full_world_335.23061_2023_06_14.7z"
+export const TDB_URL = "https://github.com/TrinityCore/TrinityCore/releases/download/TDB335.24041/TDB_full_world_335.24041_2024_04_10.7z"
 
 export const DATASET_MODULES_CONFIG = 'Dataset.Modules'
 export const DATASET_CLIENT_PATCH_LETTER = 'Client.Patch.Letter'
