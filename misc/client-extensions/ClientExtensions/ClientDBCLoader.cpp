775d4e3f900fbc95627f52847f470585bb75557f
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-01-16 09:45:01 +0100

    Smh I don't even need this part in dll

--- misc/client-extensions/ClientExtensions/ClientDBCLoader.cpp
+++
@@ -1,8 +0,0 @@
-#include "ClientDBCLoader.h"
-
-void ClientDBCLoader::RegisterDBCs(void* ptr) {
-    LOG_INFO << "Registering base DBCs";
-    // Aleist3r: for now, it only executes client function to load stock dbcs
-    RegisterBase(ptr);
-    return;
-}
--- misc/client-extensions/ClientExtensions/ClientMisc.cpp
+++ misc/client-extensions/ClientExtensions/ClientMisc.cpp
@@ -1,4 +1,5 @@
-#include "ClientDBCLoader.h"
+#include "ClientDetours.h"
+#include "Logger.h"
 #include <windows.h>
 
 // Aleist3r: this is probably one of the earliest points you can attach yourself after dll is loaded
@@ -6,9 +7,6 @@
 CLIENT_DETOUR(StartupOverrides, 0x86D0A0, __cdecl, int, ()) {
     LOG_DEBUG << "StartupOverrides executed";
     DWORD flOldProtect;
-    VirtualProtect((LPVOID)0x634E30, 0x4, PAGE_EXECUTE_READWRITE, &flOldProtect);
-    *(uint32_t*)0x634E30 = ((uint32_t)(void*)ClientDBCLoader::RegisterDBCs - (0x634E30 + 0x4));
-    VirtualProtect((LPVOID)0x634E30, 0x4, PAGE_EXECUTE_READ, &flOldProtect);
     // Aleist3r: this thing is for debugging purposes - this is unused address, I write same data
     ///VirtualProtect((LPVOID)0x6337CA, 0x5, PAGE_EXECUTE_READWRITE, &flOldProtect);
     return StartupOverrides();
