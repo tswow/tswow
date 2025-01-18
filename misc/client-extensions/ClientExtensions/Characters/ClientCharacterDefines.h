43882275004c86811e2d7be83e4bb47d9fb60c30
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-01-18 21:48:24 +0100

    Reuse client addresses

--- misc/client-extensions/ClientExtensions/Characters/ClientCharacterDefines.h
+++ misc/client-extensions/ClientExtensions/Characters/ClientCharacterDefines.h
@@ -7,25 +7,14 @@ static uint32_t memoryTable[128];
 
 static uint32_t dummy = 0;
 
-static char* sBloodElf = "BloodElf";
-static char* sDraenei = "Draenei";
-static char* sDwarf = "Dwarf";
 static char* sEredar = "Eredar";
-static char* sGnome = "Gnome";
-static char* sGoblin = "Goblin";
-static char* sHuman = "Human";
 static char* sLightforged = "Lightforged";
 static char* sNaga = "Naga";
-static char* sNightElf = "NightElf";
 static char* sNightborne = "Nightborne";
-static char* sOrc = "Orc";
 static char* sPandarenA = "Pandaren_Alliance";
 static char* sPandarenH = "Pandaren_Horde ";
 static char* sPandarenN = "Pandaren_Neutral";
 static char* sQueldo = "Queldo";
-static char* sScourge = "Scourge";
-static char* sTauren = "Tauren";
-static char* sTroll = "Troll";
 static char* sWorgen = "Worgen";
 static char* sVoidElf = "VoidElf";
 static char* sVulperaA = "Vulpera_Alliance";
@@ -35,17 +24,17 @@ static char* sZandalari = "ZandalariTroll";
 
 static uint32_t raceNameTable[32] = {
 	0,
-	reinterpret_cast<uint32_t>(&sHuman),
-	reinterpret_cast<uint32_t>(&sOrc),
-	reinterpret_cast<uint32_t>(&sDwarf),
-	reinterpret_cast<uint32_t>(&sNightElf),
-	reinterpret_cast<uint32_t>(&sScourge),
-	reinterpret_cast<uint32_t>(&sTauren),
-	reinterpret_cast<uint32_t>(&sGnome),
-	reinterpret_cast<uint32_t>(&sTroll),
-	reinterpret_cast<uint32_t>(&sGoblin),
-	reinterpret_cast<uint32_t>(&sBloodElf),
-	reinterpret_cast<uint32_t>(&sDraenei),
+	0x9E4E2C, // Human
+	0x9E4E28, // Orc
+	0x9E4E20, // Dwarf
+	0x9E4E14, // NightElf
+	0xA59634, // Scourge
+	0x9E4E04, // Tauren
+	0x9E4DFC, // Gnome
+	0x9E4DF4, // Troll
+	0xA5962C, // Goblin
+	0xA59620, // BloodElf
+	0xA59618, // Draenei
 	reinterpret_cast<uint32_t>(&sWorgen),
 	reinterpret_cast<uint32_t>(&sNaga),
 	reinterpret_cast<uint32_t>(&sPandarenA),
