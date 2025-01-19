086f404221b6f928d0770c1a337eac30fd42de0b
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-01-19 19:44:25 +0100

    Client extensions string pointers fix

--- misc/client-extensions/ClientExtensions/Characters/ClientCharacterDefines.h
+++ misc/client-extensions/ClientExtensions/Characters/ClientCharacterDefines.h
@@ -7,21 +7,6 @@ static uint32_t memoryTable[128] = { 0 };
 
 static uint32_t dummy = 0;
 
-static char* sEredar = "Eredar";
-static char* sLightforged = "Lightforged";
-static char* sNaga = "Naga";
-static char* sNightborne = "Nightborne";
-static char* sPandarenA = "Pandaren_Alliance";
-static char* sPandarenH = "Pandaren_Horde ";
-static char* sPandarenN = "Pandaren_Neutral";
-static char* sQueldo = "Queldo";
-static char* sWorgen = "Worgen";
-static char* sVoidElf = "VoidElf";
-static char* sVulperaA = "Vulpera_Alliance";
-static char* sVulperaH = "Vulpera_Horde";
-static char* sVulperaN = "Vulpera_Neutral";
-static char* sZandalari = "ZandalariTroll";
-
 static uint32_t raceNameTable[32] = {
     0,        // string pointers
     0x9E4E2C, // Human
@@ -35,20 +20,20 @@ static uint32_t raceNameTable[32] = {
     0xA5962C, // Goblin
     0xA59620, // BloodElf
     0xA59618, // Draenei
-    reinterpret_cast<uint32_t>(&sWorgen),
-    reinterpret_cast<uint32_t>(&sNaga),
-    reinterpret_cast<uint32_t>(&sPandarenA),
-    reinterpret_cast<uint32_t>(&sQueldo),
-    reinterpret_cast<uint32_t>(&sPandarenH),
-    reinterpret_cast<uint32_t>(&sNightborne),
-    reinterpret_cast<uint32_t>(&sVoidElf),
-    reinterpret_cast<uint32_t>(&sVulperaA),
-    reinterpret_cast<uint32_t>(&sVulperaH),
-    reinterpret_cast<uint32_t>(&sVulperaN),
-    reinterpret_cast<uint32_t>(&sPandarenN),
-    reinterpret_cast<uint32_t>(&sZandalari),
-    reinterpret_cast<uint32_t>(&sLightforged),
-    reinterpret_cast<uint32_t>(&sEredar),
+    reinterpret_cast<uint32_t>(&"Worgen"),
+    reinterpret_cast<uint32_t>(&"Naga"),
+    reinterpret_cast<uint32_t>(&"Pandaren_Alliance"),
+    reinterpret_cast<uint32_t>(&"Queldo"),
+    reinterpret_cast<uint32_t>(&"Pandaren_Horde "),
+    reinterpret_cast<uint32_t>(&"Nightborne"),
+    reinterpret_cast<uint32_t>(&"VoidElf"),
+    reinterpret_cast<uint32_t>(&"Vulpera_Alliance"),
+    reinterpret_cast<uint32_t>(&"Vulpera_Horde"),
+    reinterpret_cast<uint32_t>(&"Vulpera_Neutral"),
+    reinterpret_cast<uint32_t>(&"Pandaren_Neutral"),
+    reinterpret_cast<uint32_t>(&"ZandalariTroll"),
+    reinterpret_cast<uint32_t>(&"Lightforged"),
+    reinterpret_cast<uint32_t>(&"Eredar"),
     reinterpret_cast<uint32_t>(&dummy),
     reinterpret_cast<uint32_t>(&dummy),
     reinterpret_cast<uint32_t>(&dummy),
