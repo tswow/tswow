f73ffc380fa746820b46dcb64072517e95fc1429
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-01-30 12:03:20 +0100

    Oops, LOG_DEBUG no longer needed

--- misc/client-extensions/ClientExtensions/ClientLua.cpp
+++ misc/client-extensions/ClientExtensions/ClientLua.cpp
@@ -183,7 +183,6 @@ LUA_FUNCTION(GetSpellDescription, (lua_State* L)) {
         if (ClientDB__GetLocalizedRow((void*)0xAD49D0, spellId, &buffer)) { // hex address is g_SpellRec struct
             SpellParserParseText(&buffer, &dest, 1024, 0, 0, 0, 0, 1, 0);
             ClientLua::PushString(L, dest);
-            LOG_DEBUG << dest;
             return 1;
         }
     }
