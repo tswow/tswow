a49a66f3438638b2ca1a4d582c7f2e5eff2ee4bb
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-01-31 22:52:13 +0100

    Spaces

--- misc/client-extensions/ClientExtensions/CustomLua/PlayerFunctions.cpp
+++ misc/client-extensions/ClientExtensions/CustomLua/PlayerFunctions.cpp
@@ -16,10 +16,10 @@ LUA_FUNCTION(SetActiveSpec, (lua_State* L)) {
 }
 
 LUA_FUNCTION(SetMasteryRatings, (lua_State* L)) {
-    CharacterDefines::setMasteryRatingSpec(0,ClientLua::GetNumber(L, 1));
-    CharacterDefines::setMasteryRatingSpec(1,ClientLua::GetNumber(L, 2));
-    CharacterDefines::setMasteryRatingSpec(2,ClientLua::GetNumber(L, 3));
-    CharacterDefines::setMasteryRatingSpec(3,ClientLua::GetNumber(L, 4));
+    CharacterDefines::setMasteryRatingSpec(0, ClientLua::GetNumber(L, 1));
+    CharacterDefines::setMasteryRatingSpec(1, ClientLua::GetNumber(L, 2));
+    CharacterDefines::setMasteryRatingSpec(2, ClientLua::GetNumber(L, 3));
+    CharacterDefines::setMasteryRatingSpec(3, ClientLua::GetNumber(L, 4));
 
     ClientLua::PushNil(L);
     return 1;
