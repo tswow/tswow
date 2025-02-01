aeca1e57cb1e9825ccba06ab69e1af6bc45a681f
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-02-01 13:35:50 +0100

    Names

--- misc/client-extensions/ClientExtensions/CustomDBCMgr/DBCDefs/SpellAdditionalCostData.h
+++ misc/client-extensions/ClientExtensions/CustomDBCMgr/DBCDefs/SpellAdditionalCostData.h
@@ -4,26 +4,26 @@
 #include "Logger.h"
 
 struct SpellAdditionalCostDataRow {
-    int entry;
-    void* name;
-    int playerKillingAllowed;
-    int roleplaying;
+    int spellID;
+    void* resourceName;
+    int cost;
+    int flag;
 };
 
 class SpellAdditionalCostData : public CustomDBC {
 public:
     const char* fileName = "DBFilesClient\\SpellAdditionalCostData.dbc";
-     SpellAdditionalCostData() : CustomDBC(){
+    SpellAdditionalCostData() : CustomDBC() {
         this->numColumns = 4;
         this->rowSize = 16;
     }
     void LoadDB(){
         CustomDBC::LoadDB(this->fileName);
         SpellAdditionalCostDataRow* row = (SpellAdditionalCostDataRow*)CustomDBC::GetRow(2);
-        LOG_DEBUG << row->entry;
+        LOG_DEBUG << row->spellID;
         //LOG_DEBUG << (char*)this->stringTable[row->name];// idk how to get strings yet. this is just an idea.
-        LOG_DEBUG << row->playerKillingAllowed;
-        LOG_DEBUG << row->roleplaying;
+        LOG_DEBUG << row->cost;
+        LOG_DEBUG << row->flag;
         };
 };
 
