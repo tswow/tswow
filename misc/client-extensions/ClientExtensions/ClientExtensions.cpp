65c9c778ec1001bdd0db21063c286edf9b29f79d
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2025-02-14 13:08:58 +0100

    Forgot to actually load anything

--- misc/client-extensions/ClientExtensions/ClientExtensions.cpp
+++ misc/client-extensions/ClientExtensions/ClientExtensions.cpp
@@ -1,6 +1,8 @@
 #include "ClientExtensions.h"
+#include "CustomDBCMgr/CustomDBCMgr.h"
 
 void ClientExtensions::initialize() {
+    CustomDBCMgr::Load();
     CharacterFixes::CharacterCreationFixes();
     CharacterExtensions::Apply();
     SpellTooltipExtensions::Apply();
